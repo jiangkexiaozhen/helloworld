26fea298f8abab409b15564a3cab383ff26629c1
jiangke <jiangkexiaozhen2012@gmail.com>
2018-01-31 18:39:13 +0800

    0002

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.awt.*;
-import java.util.Iterator;
-import java.util.ArrayList;
+import java.util.Iterator;  // 迭代器
+import java.util.ArrayList; //矩阵序列
+import java.util.Date;      // 导入 java.util 包下的 Date 类
+import java.util.Scanner;   // 导入 java.util 包下的 Scanner 类
+import javax.swing.*;       // 导入 javax.swing 包下的所有类，* 表示所有类
 
 public class Main {
 
@@ -21,12 +24,35 @@ public class Main {
         jouirSchool.name = "Paul Lee";
         jouirSchool.age  = 10;
         jouirSchool.score= (float)(99.5);
-        jouirSchool.serialNumber.add("93900300939") ;
-        jouirSchool.serialNumber.add("93393993939") ;
-        jouirSchool.serialNumber.add("933993939x1") ;
-
+        jouirSchool.serialNumber.add("93900300939") ;;
         jouirSchool.say();
+
+         class Dog{
+            String name;
+            int age;
+            // 构造方法，没有返回值
+            Dog(String name1, int age1){
+                name = name1;
+                age = age1;
+                System.out.println("感谢主人领养了我");
+            }
+            // 普通方法，必须有返回值
+            void bark(){
+                System.out.println("汪汪，不要过来");
+            }
+            void hungry(){
+                System.out.println("主人，我饿了");
+            }
+            //public void main(String arg[]){
+                // 创建对象时传递的参数要与构造方法参数列表对应
+            //}
+        }
+        Dog myDog = new Dog("花花", 3);
+        myDog.bark();
+        myDog.hungry();
 
         System.out.println("file end!");
     }
+
+
 }
